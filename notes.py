5ec95a3389271bf50c252874ca01de1f2c9caaf2
Daniel Kassaye <danielkassaye@daniels-air-4.mynetworksettings.com>
2024-08-09 16:20:39 -0700

    first commit

---
+++ notes.py
@@ -0,0 +1,41 @@
+'''
+
+This is a note for Module 5: Relational Databases
+This is a Multi-line String comment. 
+
+LESSON 1: Database Fundamentals
+
+'''
+#Database is a file that is organized for storing data. 
+
+# Relational Database --> a database structured to recognize relations among stored items of information.
+# Data is structured in rows and columns
+# order reigns supreme
+# Primary Keys are unique
+#-----------------------------------------------------------------------------------------------------------------------------
+# Non-Relational Database --> Mix of structures, Data lounging in documents, key-value pairs, and wide columns that stretch as
+# far as the eye can see. 
+# Provides more flexibility
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Relational Database --> Tables --> Rows --> Columns  
+
+# Tables --> Rows and Columns 
+
+#Intro to Keys -- Primary and Foriegn Keys with Indexing
+
+# Constraints --> Unique and Non-Null
+# Default Value
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#Database Management Systems (DBMS) -->
+# MySQL
+# PostgreSQL
+# Oracle
+# SQLite
+
+
+#Entity-Relationship Diagrams (ERDs) --> like maps (of a city)
+# Components of ERD --> Entities, Attributes, and Relationships
+x = 1 
+print(x)
